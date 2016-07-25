021b790e91a6f2a69abbe120a3d650fec42d525f
Ferry Temaja Putra <Ferry Temaja Putra>
2016-07-25 20:24:47 +0800

    add product models

---
+++ api/models/Productread.js
@@ -0,0 +1,20 @@
+/**
+ * Productread.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    product: 'integer',
+    name: 'string',
+    description: 'string',
+    category: 'string',
+    size: 'string',
+    color: 'string',
+    price: 'float'
+  }
+};
+
