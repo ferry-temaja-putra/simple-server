70436925948dde5355213ffff65146089054e562
Ferry Temaja Putra <Ferry Temaja Putra>
2016-07-25 20:49:56 +0800

    add product repository

---
+++ api/repositories/ProductRepository.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    addProduct: function (newProduct, callback) {
+        Product.create(newProduct).exec(function (err, created) {
+            if (err) return callback(err);
+            return callback(null, created);
+        });
+    },
+
+    addInventory: function (product, newInventory, callback) {        
+        Inventory.create(newInventory).exec(function (err, created) {
+           if (err) return callback(err);
+           return callback(null, created); 
+        });
+    },
+
+    addProductForRead: function (newProductForRead, callback) {
+        Productread.create(newProductForRead).exec(function (err, created) {
+            if (err) return callback(err);
+            return callback(null, created);
+        });
+    }
+};
