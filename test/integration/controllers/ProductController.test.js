0cf2d96d787bb1039946a1913bf73e4715e18a57
Ferry Temaja Putra <Ferry Temaja Putra>
2016-07-26 12:50:45 +0800

    add test for addInventory api

--- test/integration/controllers/ProductController.test.js
+++ test/integration/controllers/ProductController.test.js
@@ -71,4 +71,40 @@ describe('ProductController', function() {
             });            
         });
     });
+
+    describe('#addInventory()', function() {
+        it('should return the created inventory', function (done) {
+            Category.create({name: 'test'}).exec(function (err, createdCategory) {
+                if (err) done(err);
+
+                Product.create({name: 'test', category: createdCategory.id}).exec(function (err, createdProduct) {
+                    request(sails.hooks.http.app)
+                    .post('/product/addInventory')
+                    .send({ product: createdProduct.id, size: 'medium', color: 'blue', price: 90000})
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) return done(err);
+                        // clean up the created data
+                        Product.destroy({id: createdProduct.id}).exec(function (err) {
+                            if (err) return done(err);
+
+                            Category.destroy({id: createdCategory.id}).exec(function (err) {
+                                if (err) return done(err);
+
+                                Inventory.destroy({id: res.body.id}).exec(function (err) {
+                                    if (err) return done(err);
+
+                                    Productread.destroy({product: createdProduct.id}).exec(function (err) {
+                                        if (err) return done(err);
+                                        done();
+                                    })
+                                });
+                            });
+                        });
+                    });                       
+                }); 
+            });            
+        });
+    });
 });
