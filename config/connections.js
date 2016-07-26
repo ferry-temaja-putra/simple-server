6b6a30da845d720563200dfbc68b383649c62154
Ferry Temaja Putra <Ferry Temaja Putra>
2016-07-26 19:35:51 +0800

    uninstall postgresql

--- config/connections.js
+++ config/connections.js
@@ -74,13 +74,13 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
-  PostgresqlServer: {
-    adapter: 'sails-postgresql',
-    host: 'ec2-54-243-203-93.compute-1.amazonaws.com',
-    user: 'kifydpglxqxhvt', // optional
-    password: 'LrZP4mPAFDDKpp6jfWWYTepScX', // optional
-    database: 'd9miuqug9s52rq' //optional
-  }
+  // PostgresqlServer: {
+  //   adapter: 'sails-postgresql',
+  //   host: 'ec2-54-243-203-93.compute-1.amazonaws.com',
+  //   user: 'kifydpglxqxhvt', // optional
+  //   password: '', // optional
+  //   database: 'd9miuqug9s52rq' //optional
+  // }
 
 
   /***************************************************************************
