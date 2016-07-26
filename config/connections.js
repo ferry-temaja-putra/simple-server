cb96a80bf8448c7f5fa56d8ddf09d44631d402f4
Ferry Temaja Putra <Ferry Temaja Putra>
2016-07-26 19:43:15 +0800

    install mongodb adapter

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,14 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  MongodbServer: {
+    adapter: 'sails-mongo',
+    host: 'ds049211.mlab.com',
+    port: 49211,
+    user: 'dbuser', //optional
+    password: 'dbuser1', //optional
+    database: 'simple-server' //optional
+  },
 
   /***************************************************************************
   *                                                                          *
